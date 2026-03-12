e4748b25d396848ee191748b26bf51ee9f89b62f
Gurusankar <your-github-gurusankar.2153020@srec.ac.in>
2026-03-12 13:52:41 +0530

    images

--- uploadhtml.js
+++ uploadhtml.js
@@ -5,12 +5,14 @@ const axios = require("axios");
 const htmlContent = fs.readFileSync("output.html", "utf8");
 
 // API URL given by interviewer
-const API_URL =
-  "TvrDSpEEHxJVgfim9Gbqpw9ZZ5Vz2XwCFnPFqQ2DMFBl/A+ZI4PAfRO9qOGyG14nflFJ5n/8HuMsM2wiNzlBmT0QTuT1ktlNh0ueEqCKaRaSwygmVAbl3u5oKyyt3IYcKTLXz8XbxfYcXyG36rCFvA==";
+const API_URL = "https://apihub.document360.io/v2/Articles";
 
 // request body
 const body = {
   content: htmlContent,
+  project_version_id: "8de99e99-94af-4315-9047-8fd5f90b2f8d", // add your project version id here
+  user_id: "67890", // add your user id here
+  category_id: "12345", // add your category id here
 };
 
 axios
@@ -25,5 +27,9 @@ axios
     console.log("Success:", res.data);
   })
   .catch((err) => {
-    console.log("Error:", err.response.data);
+    if (err.response) {
+      console.log("Error:", err.response.data);
+    } else {
+      console.log("Error:", err.message);
+    }
   });
